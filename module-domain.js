33cedfcddb03e75171a0ea3c21817480a21c4405
benlv <benlv.hu@dianrong.com>
2017-07-03 16:40:22 +0800

    Domain Env Delete

--- module-domain.js
+++ module-domain.js
@@ -4,189 +4,233 @@ var ObjectId = require('mongodb').ObjectId;
 var uuidv1 = require('uuid/v1');
 
 module.exports = {
-	init: function(app){
+    init: function (app) {
 
 		// 查询Domain列表
-		app.get('/domain', function (req, res) {
-			var params = req.query;
-			if(!params.page || !params.pageSize){
-				res.status(400).send('Bad Request! Required Parameters: page and pageSize');
-			}
+        app.get('/domain', function (req, res) {
+            var params = req.query;
 
-			var criteria = {};
-			if(params.name){
-				criteria = { "$or": [
-				    { "name": { "$regex": new RegExp(params.name)} },
-				    { "description": { "$regex": new RegExp(params.name) }},
-				    { "email": { "$regex": new RegExp(params.name) }}
-				]};
-			}
+            if (!params.page || !params.pageSize) {
+                res.status(400).send('Bad Request! Required Parameters: page and pageSize');
+            }
 
-			DAO.find(criteria, params.page, params.pageSize, function(err, docs, totalSize){
-				if(err){
-					res.status(500);
-					return;
-				}
-				return res.json(new PaginationResponse(docs, params.page, params.pageSize,totalSize));
-			});
-    });
+            var criteria = {};
+
+            if (params.name) {
+                criteria = { '$or': [
+											{ 'name': { '$regex': new RegExp(params.name) } },
+											{ 'description': { '$regex': new RegExp(params.name) } },
+											{ 'email': { '$regex': new RegExp(params.name) } }
+                ] };
+            }
+
+            DAO.find(criteria, params.page, params.pageSize, function (err, docs, totalSize) {
+                if (err) {
+                    res.status(500);
+                    return;
+                }
+                return res.json(new PaginationResponse(docs, params.page, params.pageSize, totalSize));
+            });
+        });
 		// 接入Domain
-		app.put('/domain', function (req, res) {
+        app.put('/domain', function (req, res) {
 
-			if(!req.body.name
+            if (!req.body.name
 				|| !req.body.email
 				|| !req.body.description
 				|| !req.body.endDateTime
-				|| !req.body.starDateTime){
-				res.status(400).send('Bad Request! Required Parameters: name, email, endDateTime, starDateTime, description');
-				return;
-			}
+				|| !req.body.starDateTime) {
+                res.status(400).send('Bad Request! Required Parameters: name, email, endDateTime, starDateTime, description');
+                return;
+            }
 
-			DAO.find({
-					"name": req.body.name
-			}, 1, 1, function(err, docs, totalSize){
-				if(err){
-					res.status(500);
-					return;
-				}
-				if(totalSize){
-					res.status(500).send({errMsg:req.body.name+'已存在'});
-					return;
-				}
+            DAO.find({
+                'name': req.body.name
+            }, 1, 1, function (err, docs, totalSize) {
+                if (err) {
+                    res.status(500);
+                    return;
+                }
+                if (totalSize) {
+                    res.status(500).send({ errMsg: req.body.name + '已存在' });
+                    return;
+                }
 
-				var timestamp = new Date().getTime();
-				var entity = {
-					name: req.body.name,
-					description: req.body.description,
-					email: req.body.email,
-					email: req.body.email,
-					endDateTime: req.body.endDateTime,
-					starDateTime: req.body.starDateTime,
-					createdTime: timestamp,
-					updatedTime: timestamp,
-					enabled: true
-				}
-				DAO.add(entity, function(err, result){
-					if(err){
-						res.status(500);
-						return;
-					}
-					res.json(entity);
-				});
+                var timestamp = new Date().getTime();
+                var entity = {
+                    name: req.body.name,
+                    description: req.body.description,
+                    email: req.body.email,
+                    endDateTime: req.body.endDateTime,
+                    starDateTime: req.body.starDateTime,
+                    createdTime: timestamp,
+                    updatedTime: timestamp,
+                    enabled: true
+                };
 
-			});
-    });
+                DAO.add(entity, function (e) {
+                    if (e) {
+                        res.status(500);
+                        return;
+                    }
+                    res.json(entity);
+                });
+
+            });
+        });
 
 		// Get Domain Detail
-		app.get('/domain/:id', function (req, res) {
-			DAO.findById(req.params.id, function(err, doc){
-				if(err){
-						res.status(500);
-						return;
-				}
-				res.json(doc?doc:{});
-			});
-		});
+        app.get('/domain/:id', function (req, res) {
+            DAO.findById(req.params.id, function (err, doc) {
+                if (err) {
+                    res.status(500);
+                    return;
+                }
+                res.json(doc ? doc : {});
+            });
+        });
 
 		// 添加部署环境
-		app.put('/domain/:domainId/env', function (req, res) {
+        app.put('/domain/:domainId/env', function (req, res) {
 
-			if(!req.body.name
+            if (!req.body.name
 				|| !req.body.email
 				|| !req.body.description
 				|| !req.body.logLevel
-				|| !req.body.domainId){
-				res.status(400).send('Bad Request! Required Parameters: name, email, description, logLevel, domainId');
-				return;
-			}
+				|| !req.body.domainId) {
+                res.status(400).send('Bad Request! Required Parameters: name, email, description, logLevel, domainId');
+                return;
+            }
 
-			var domainId = req.body.domainId;
-			var entity = {
-				id: uuidv1(),
-				name: req.body.name,
-				email: req.body.email,
-				description: req.body.description,
-				logLevel: req.body.logLevel
-			};
+            var domainId = req.body.domainId;
+            var entity = {
+                id: uuidv1(),
+                name: req.body.name,
+                email: req.body.email,
+                description: req.body.description,
+                logLevel: req.body.logLevel
+            };
 
-			DAO.findById(domainId, function(err, doc){
-				if(err){
-						res.status(500);
-						return;
-				}
-				var isAlreadyExists = false;
-				if(doc.env){
-					isAlreadyExists = doc.env.some(function(env){
-						return env.name.toLowerCase() === entity.name.toLowerCase();
-					})
-				}
-				if(isAlreadyExists){
-					res.status(500).send({errMsg:'添加失败：部署环境'+entity.name + '已存在'});
-					return;
-				}
-				DAO.updateOne({
-					_id: new ObjectId(domainId)
-				},{
-					$set: { env : doc.env?doc.env.concat(entity):[entity] }
-				}, function(err, result){
-					if(err){
-							res.status(500);
-							return;
-					}
-					res.sendStatus(204);
-				});
-			});
+            DAO.findById(domainId, function (err, doc) {
+                if (err) {
+                    res.status(500);
+                    return;
+                }
+                var isAlreadyExists = false;
 
-		});
+                if (doc.env) {
+                    isAlreadyExists = doc.env.some(function (env) {
+                        return env.name.toLowerCase() === entity.name.toLowerCase();
+                    });
+                }
+                if (isAlreadyExists) {
+                    res.status(500).send({ errMsg: '添加失败：部署环境' + entity.name + '已存在' });
+                    return;
+                }
+                DAO.updateOne({
+                    _id: new ObjectId(domainId)
+                }, {
+                    $set: { env: doc.env ? doc.env.concat(entity) : [entity] }
+                }, function (e) {
+                    if (e) {
+                        res.status(500);
+                        return;
+                    }
+                    res.sendStatus(204);
+                });
+            });
 
+        });
+		// 删除部署环境
+        app.delete('/domain/:domainId/env/:envId', function (req, res) {
+            var params = req.params;
+
+            if (!params.domainId || !params.envId) {
+                res.status(400).send('Bad Request! Required Parameters: domainId, envId');
+            }
+
+            DAO.findById(params.domainId, function (err, doc) {
+                if (err) {
+                    res.status(500);
+                    return;
+                }
+                var currentEnv, currentIndex;
+
+                if (doc.env) {
+                    currentEnv = doc.env.find(function (env, index) {
+                        currentIndex = index;
+                        return env.id.toLowerCase() === params.envId.toLowerCase();
+                    });
+                }
+                if (!currentEnv) {
+                    res.status(204);
+                    return;
+                }
+
+                doc.env.splice(currentIndex, 1);
+
+                DAO.updateOne({
+                    _id: new ObjectId(params.domainId)
+                }, {
+                    $set: { env: doc.env }
+                }, function (e) {
+                    if (e) {
+                        res.status(500);
+                        return;
+                    }
+                    res.sendStatus(204);
+                });
+            });
+        });
 		// 修改部署环境
-		app.post('/domain/:domainId/env/:envId', function (req, res) {
+        app.post('/domain/:domainId/env/:envId', function (req, res) {
 
-			if(!req.body.name
+            if (!req.body.name
 				|| !req.body.email
 				|| !req.body.description
 				|| !req.body.logLevel
-				|| !req.params.envId){
-				res.status(400).send('Bad Request! Required Parameters: name, email, description, logLevel');
-				return;
-			}
+				|| !req.params.envId) {
+                res.status(400).send('Bad Request! Required Parameters: name, email, description, logLevel');
+                return;
+            }
 
-			var domainId = req.params.domainId;
-			var envId = req.params.envId;
+            var domainId = req.params.domainId;
+            var envId = req.params.envId;
 
-			DAO.findById(domainId, function(err, doc){
-				if(err){
-						res.status(500);
-						return;
-				}
-				var currentEnv;
-				if(doc.env){
-					currentEnv = doc.env.find(function(env){
-						return env.id.toLowerCase() === envId.toLowerCase();
-					})
-				}
-				if(!currentEnv){
-					res.status(500).send({errMsg:'修改失败：部署环境'+req.body.name + '不存在，已被删除！'});
-					return;
-				}
-				currentEnv.name = req.body.name;
-				currentEnv.email = req.body.email;
-				currentEnv.description = req.body.description;
-				currentEnv.logLevel = req.body.logLevel;
+            DAO.findById(domainId, function (err, doc) {
+                if (err) {
+                    res.status(500);
+                    return;
+                }
+                var currentEnv;
 
-				DAO.updateOne({
-					_id: new ObjectId(domainId)
-				},{
-					$set: { env : doc.env }
-				}, function(err, result){
-					if(err){
-						res.status(500);
-							return;
-					}
-					res.sendStatus(204);
-				});
-			});
+                if (doc.env) {
+                    currentEnv = doc.env.find(function (env) {
+                        return env.id.toLowerCase() === envId.toLowerCase();
+                    });
+                }
+                if (!currentEnv) {
+                    res.status(500).send({ errMsg: '修改失败：部署环境' + req.body.name + '不存在，已被删除！' });
+                    return;
+                }
+                currentEnv.name = req.body.name;
+                currentEnv.email = req.body.email;
+                currentEnv.description = req.body.description;
+                currentEnv.logLevel = req.body.logLevel;
 
-		});
-	}
-}
+                DAO.updateOne({
+                    _id: new ObjectId(domainId)
+                }, {
+                    $set: { env: doc.env }
+                }, function (e) {
+                    if (e) {
+                        res.status(500);
+                        return;
+                    }
+                    res.sendStatus(204);
+                });
+            });
+
+        });
+    }
+};
--- module-user.js
+++ module-user.js
@@ -1,141 +1,146 @@
 var DAO = require('./service/user');
-var InternalErrorResponse = require('./entity/InternalErrorResponse');
-var ObjectResponse = require('./entity/ObjectResponse');
 var PaginationResponse = require('./entity/PaginationResponse');
 
 module.exports = {
-    init: function(app) {
+    init: function (app) {
         app.get('/domain/:domainId/env/:env/user', function (req, res) {
-          var query = req.query;
-          var params = req.params;
-          if(!params.domainId || !params.env || !query.page || !query.pageSize){
-            res.status(400).send('Bad Request! Required Parameters: domainId, env, page and pageSize');
-          }
+            var query = req.query;
+            var params = req.params;
 
-          var criteria = {
-            "domainId": params.domainId,
-            "env": params.env
-          };
-          if(query.email){
-            criteria['$or'] = [
-              { "name": { "$regex": new RegExp(query.email)} },
-              { "email": { "$regex": new RegExp(query.email)} }
-            ];
-          }
-          var page = parseInt(query.page);
-          var pageSize = parseInt(query.pageSize);
-
-          DAO.find(criteria, page, pageSize, function(err, docs, totalSize){
-            if(err){
-              res.status(500);
-              return;
-            }
-            return res.json(new PaginationResponse(docs, page, pageSize,totalSize));
-          });
-        });
-
-        app.put('/domain/:domainId/env/:env/user', function(req, res) {
-          var params = req.params;
-          var body = req.body;
-          if(!params.domainId || !params.env || !body.name || !body.email || !body.status || !body.userType){
-            res.status(400).send('Bad Request! Required Parameters: domainId, env, name, status, userType and email');
-          }
-
-          DAO.find({
-              "domainId": params.domainId,
-              "env": params.env,
-              "email": body.email
-          }, 1, 1, function(err, docs, totalSize){
-            if(err){
-              res.status(500);
-              return;
-            }
-            if(totalSize){
-              res.status(500).send({errMsg:'用户'+body.email+'已存在'});
-              return;
+            if (!params.domainId || !params.env || !query.page || !query.pageSize) {
+                res.status(400).send('Bad Request! Required Parameters: domainId, env, page and pageSize');
             }
 
-            var timestamp = new Date().getTime();
-            var roleEntity = {
-              name: body.name,
-              email: body.email,
-              createDate: timestamp,
-              lastUpdate: timestamp,
-              status: body.status,
-              userType: body.userType,
-              domainId: params.domainId,
-              env: params.env
+            var criteria = {
+                'domainId': params.domainId,
+                'env': params.env
+            };
+
+            if (query.email) {
+                criteria.$or = [
+              { 'name': { '$regex': new RegExp(query.email) } },
+              { 'email': { '$regex': new RegExp(query.email) } }
+                ];
             }
-            DAO.add(roleEntity, function(err, result){
-              if(err){
-                res.status(500);
-                return;
-              }
-              res.sendStatus(204);
+            var page = parseInt(query.page, 10);
+            var pageSize = parseInt(query.pageSize, 10);
+
+            DAO.find(criteria, page, pageSize, function (err, docs, totalSize) {
+                if (err) {
+                    res.status(500);
+                    return;
+                }
+                return res.json(new PaginationResponse(docs, page, pageSize, totalSize));
             });
-
-          });
         });
 
-        app.delete('/domain/:domainId/env/:env/user', function(req, res) {
-          var params = req.params;
-          var query = req.query;
-          if(!params.domainId || !params.env || !query.email){
-            res.status(400).send('Bad Request! Required Parameters: domainId, env and email');
-          }
+        app.put('/domain/:domainId/env/:env/user', function (req, res) {
+            var params = req.params;
+            var body = req.body;
 
-          DAO.delete({
-              "domainId": params.domainId,
-              "env": params.env,
-              "email": query.email
-          }, function(err, result){
-            if(err){
-              res.status(500);
-              return;
+            if (!params.domainId || !params.env || !body.name || !body.email || !body.status || !body.userType) {
+                res.status(400).send('Bad Request! Required Parameters: domainId, env, name, status, userType and email');
             }
-            res.sendStatus(204);
-          });
+
+            DAO.find({
+                'domainId': params.domainId,
+                'env': params.env,
+                'email': body.email
+            }, 1, 1, function (err, docs, totalSize) {
+                if (err) {
+                    res.status(500);
+                    return;
+                }
+                if (totalSize) {
+                    res.status(500).send({ errMsg: '用户' + body.email + '已存在' });
+                    return;
+                }
+
+                var timestamp = new Date().getTime();
+                var roleEntity = {
+                    name: body.name,
+                    email: body.email,
+                    createDate: timestamp,
+                    lastUpdate: timestamp,
+                    status: body.status,
+                    userType: body.userType,
+                    domainId: params.domainId,
+                    env: params.env
+                };
+
+                DAO.add(roleEntity, function (e) {
+                    if (e) {
+                        res.status(500);
+                        return;
+                    }
+                    res.sendStatus(204);
+                });
+
+            });
         });
 
-        app.post('/domain/:domainId/env/:env/user', function(req, res) {
-          var params = req.params;
-          var body = req.body;
-          if(!params.domainId || !params.env || !body.name || !body.email || !body.status || !body.userType){
-            res.status(400).send('Bad Request! Required Parameters: domainId, env, userId, name, status, userType and email');
-          }
+        app.delete('/domain/:domainId/env/:env/user', function (req, res) {
+            var params = req.params;
+            var query = req.query;
 
-          var criteria = {
-              "domainId": params.domainId,
-              "env": params.env,
-              "email": body.email
-          };
-          DAO.find(criteria, 1, 1, function(err, docs, totalSize){
-            if(err){
-              res.status(500);
-              return;
-            }
-            if(!totalSize){
-              res.status(500).send({errMsg:'用户'+body.email+'不存在'});
-              return;
+            if (!params.domainId || !params.env || !query.email) {
+                res.status(400).send('Bad Request! Required Parameters: domainId, env and email');
             }
 
-            DAO.updateOne(criteria,{
-    					$set: {
-                name: body.name,
-                lastUpdate: new Date().getTime(),
-                status: body.status,
-                userType: body.userType,
-              }
-    				}, function(err, result){
-    					if(err){
-    							res.status(500)
-    							return;
-    					}
-    					res.sendStatus(204);
-    				});
+            DAO.delete({
+                'domainId': params.domainId,
+                'env': params.env,
+                'email': query.email
+            }, function (err) {
+                if (err) {
+                    res.status(500);
+                    return;
+                }
+                res.sendStatus(204);
+            });
+        });
 
-          });
+        app.post('/domain/:domainId/env/:env/user', function (req, res) {
+            var params = req.params;
+            var body = req.body;
+
+            if (!params.domainId || !params.env || !body.name || !body.email || !body.status || !body.userType) {
+                res.status(400).send('Bad Request! Required Parameters: domainId, env, userId, name, status, userType and email');
+            }
+
+            var criteria = {
+                'domainId': params.domainId,
+                'env': params.env,
+                'email': body.email
+            };
+
+            DAO.find(criteria, 1, 1, function (err, docs, totalSize) {
+                if (err) {
+                    res.status(500);
+                    return;
+                }
+                if (!totalSize) {
+                    res.status(500).send({ errMsg: '用户' + body.email + '不存在' });
+                    return;
+                }
+
+                DAO.updateOne(criteria, {
+                    $set: {
+                        name: body.name,
+                        lastUpdate: new Date().getTime(),
+                        status: body.status,
+                        userType: body.userType
+                    }
+                }, function (e) {
+                    if (e) {
+                        res.status(500);
+                        return;
+                    }
+                    res.sendStatus(204);
+                });
+
+            });
         });
 
     }
-}
+};
