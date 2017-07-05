45f37daaa00621d05acc00375a32e5e15b451d7d
benlv <benlv.hu@dianrong.com>
2017-07-05 12:05:22 +0800

    Login

--- app.js
+++ app.js
@@ -2,8 +2,12 @@ var bodyParser = require('body-parser');
 // var session = require('express-session');
 var allowCrossDomain  = require('./middleware/cors');
 var express = require('express');
+var redis = require('redis');
+var session = require('express-session');
+var RedisStore = require('connect-redis')(session);
 var pkg = require('./package.json');
 var DEFAULT_PORT = pkg.port;
+var client;
 
 function connectMongoDB(callback) {
     var MongoClient = require('mongodb').MongoClient;
@@ -21,8 +25,8 @@ function connectMongoDB(callback) {
 }
 
 function connectRedis(callback) {
-    var redis = require('redis');
-    var client = redis.createClient(global.AppConfig.redis.port, global.AppConfig.redis.host);
+
+    client = redis.createClient(global.AppConfig.redis.port, global.AppConfig.redis.host);
 
     client.on('connect', function () {
         console.info('Connect to redis server ' + global.AppConfig.redis.host + ' on port:' + global.AppConfig.redis.port);
@@ -34,14 +38,22 @@ function startAppServer(db) {
 
     var app = express();
 
-    app.use(bodyParser.urlencoded({ extended: true })); // for parsing application/x-www-form-urlencoded
+    app.use(session({
+        secret: global.AppConfig.session.privateKey,
+      // create new redis store.
+        store: new RedisStore({ host: global.AppConfig.redis.host, port: global.AppConfig.redis.port, client: client, ttl: 260 }),
+        saveUninitialized: false,
+        resave: false
+    }));
     app.use(bodyParser.json()); // for parsing application/json
+    app.use(bodyParser.urlencoded({ extended: true })); // for parsing application/x-www-form-urlencoded
     app.use(allowCrossDomain);
 
     global.DB = db;
     require('./module/domain').init(app, db);
     require('./module-group').init(app, db);
     require('./module/user').init(app, db);
+    require('./module/actor').init(app, db);
     require('./module-login').init(app, db);
     require('./module-location').init(app, db);
     require('./module-menu').init(app, db);
---
+++ module/actor.js
@@ -0,0 +1,44 @@
+var Member = require('../service/member');
+var md5 = require('md5');
+
+module.exports = {
+    init: function (app) {
+        app.post('/actor', function (req, res) {
+            var body = req.body;
+
+            if (!body.username || !body.password) {
+                res.status(400).send('Bad Request! Required Parameters: username, password');
+            }
+
+            var criteria = {
+                'email': body.username,
+                'password': md5(body.password)
+            };
+
+            Member.find(criteria, 1, 1, function (err, docs, totalSize) {
+                if (err) {
+                    res.status(500);
+                    return;
+                }
+                if (totalSize) {
+                    req.session.email = body.username;
+                    return res.json({
+                        email: body.username
+                    });
+                } else {
+                    res.status(500).send({ errMsg: '用户名或密码错误' });
+                }
+            });
+        });
+
+        app.get('/actor', function (req, res) {
+            if (req.session.email) {
+                return res.json({
+                    email: req.session.email
+                });
+            } else {
+                return res.json({});
+            }
+        });
+    }
+};
