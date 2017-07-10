deea44d6e9bc6473d67941b139feb7cf7eb6ea76
benlv <benlv.hu@dianrong.com>
2017-07-10 18:15:52 +0800

    Permission Sql

--- config/init-data.sql
+++ config/init-data.sql
@@ -1,15 +1,19 @@
-INSERT INTO permissions (name, type, description) VALUES ('添加角色', '角色管理','添加角色');
-INSERT INTO permissions (name, type, description) VALUES ('修改角色', '角色管理', '修改角色');
-INSERT INTO permissions (name, type, description) VALUES ('删除角色', '角色管理', '删除角色');
+INSERT INTO permissions (name, type, description) VALUES ('添加角色', '角色管理','添加角色信息');
+INSERT INTO permissions (name, type, description) VALUES ('修改角色', '角色管理', '修改角色信息');
+INSERT INTO permissions (name, type, description) VALUES ('删除角色', '角色管理', '删除角色信息');
+INSERT INTO permissions (name, type, description) VALUES ('查看角色', '角色管理', '查看角色信息');
 
-INSERT INTO permissions (name, type, description) VALUES ('添加组', '组管理','添加组');
-INSERT INTO permissions (name, type, description) VALUES ('修改组', '组管理', '修改组');
-INSERT INTO permissions (name, type, description) VALUES ('删除组', '组管理', '删除组');
+INSERT INTO permissions (name, type, description) VALUES ('添加组', '组管理','添加组信息');
+INSERT INTO permissions (name, type, description) VALUES ('修改组', '组管理', '修改组信息');
+INSERT INTO permissions (name, type, description) VALUES ('删除组', '组管理', '删除组信息');
+INSERT INTO permissions (name, type, description) VALUES ('查看组', '组管理', '查看组信息');
 
-INSERT INTO permissions (name, type, description) VALUES ('添加域', '域管理','添加域');
-INSERT INTO permissions (name, type, description) VALUES ('修改域', '域管理', '修改域');
-INSERT INTO permissions (name, type, description) VALUES ('删除域', '域管理', '删除域');
+INSERT INTO permissions (name, type, description) VALUES ('添加域', '域管理','添加域信息');
+INSERT INTO permissions (name, type, description) VALUES ('修改域', '域管理', '修改域信息');
+INSERT INTO permissions (name, type, description) VALUES ('删除域', '域管理', '删除域信息');
+INSERT INTO permissions (name, type, description) VALUES ('查看域', '域管理', '查看域信息');
 
-INSERT INTO permissions (name, type, description) VALUES ('添加用户', '用户管理','添加用户');
-INSERT INTO permissions (name, type, description) VALUES ('修改用户', '用户管理', '修改用户');
-INSERT INTO permissions (name, type, description) VALUES ('删除用户', '用户管理', '删除用户');
+INSERT INTO permissions (name, type, description) VALUES ('添加用户', '用户管理','添加用户信息');
+INSERT INTO permissions (name, type, description) VALUES ('修改用户', '用户管理', '修改用户信息');
+INSERT INTO permissions (name, type, description) VALUES ('删除用户', '用户管理', '删除用户信息');
+INSERT INTO permissions (name, type, description) VALUES ('查看用户', '用户管理', '查看用户信息');
