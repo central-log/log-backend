2579c3547007d525b074393c9a92bbc3fd91bacb
benlv <benlv.hu@dianrong.com>
2017-07-12 17:06:52 +0800

    Cluster Command

--- README.md
+++ README.md
@@ -3,3 +3,6 @@ https://docs.mongodb.com/manual/tutorial/install-mongodb-on-ubuntu/
 
 API
 2. https://docs.mongodb.com/manual/reference/program/mongod/#bin.mongod
+
+How to set up a MongoDB Sharded Cluster
+3. http://www.mongodbspain.com/en/2015/01/26/how-to-set-up-a-mongodb-sharded-cluster/
--- mongo-cluster-start.sh
+++ mongo-cluster-start.sh
@@ -1,11 +1,33 @@
 workingdir='/home/nb073/benlv/cloudLog/binary/mongodb-linux-x86_64-ubuntu1404-3.4.2'
 nodedirectory='node-'
-
-for i in {1..10}
+basePort=27000
+mongosPort=26061
+currentHost='R9P6XYZ'
+for i in {1..3}
 	do
     cd $workingdir
-		mongo_port=`expr 10000 + $i`
+		mongo_port=$((basePort+i))
+		echo $mongo_port
 		nodeid=$nodedirectory$i
-		echo "$workingdir/bin/mongod --dbpath=$workingdir/mongo-nodes/$nodeid/data --logpath=$workingdir/mongo-nodes/$nodeid/log/mongo.log --fork --port=$mongo_port --replSet cloudlog"
-		$workingdir/bin/mongod --dbpath=$workingdir/mongo-nodes/$nodeid/data --logpath=$workingdir/mongo-nodes/$nodeid/log/mongo.log --fork --port=$mongo_port --replSet cloudlog
+		$workingdir/bin/mongod --configsvr --dbpath=$workingdir/mongo-nodes/$nodeid/data --logpath=$workingdir/mongo-nodes/$nodeid/log/mongo.log --logappend --fork --port=$mongo_port
 	done
+
+for i in {4..6}
+	do
+    cd $workingdir
+		mongo_port=$((basePort+i))
+		echo $mongo_port
+		nodeid=$nodedirectory$i
+		$workingdir/bin/mongod --shardsvr --replSet cloudlog-a --dbpath=$workingdir/mongo-nodes/$nodeid/data --logpath=$workingdir/mongo-nodes/$nodeid/log/mongo.log --fork --port=$mongo_port
+	done
+
+for i in {7..9}
+	do
+    cd $workingdir
+		mongo_port=$((basePort+i))
+		echo $mongo_port
+		nodeid=$nodedirectory$i
+		$workingdir/bin/mongod --shardsvr --replSet cloudlog-b --dbpath=$workingdir/mongo-nodes/$nodeid/data --logpath=$workingdir/mongo-nodes/$nodeid/log/mongo.log --fork --port=$mongo_port
+	done
+
+$workingdir/bin/mongos --configdb $currentHost:$((basePort+1)),$currentHost:$((basePort+2)),$currentHost:$((basePort+3)) --fork --logappend --logpath $workingdir/mongo-nodes/shardedcluster.log --port $mongosPort
