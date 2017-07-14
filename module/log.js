72787355058c0cf1a5e280545dc599f2d9c1bbc9
benlv <benlv.hu@dianrong.com>
2017-07-14 12:07:39 +0800

    Log Auth && Save Log

--- module/log.js
+++ module/log.js
@@ -9,7 +9,7 @@ module.exports = {
             var body = req.body;
 
             if (!body.username || !body.password || !body.envName) {
-                res.status(400).send('Bad Request! Required Parameters: username, password');
+                res.status(400).send('Bad Request! Required Parameters: username, password, envName');
                 return;
             }
 
@@ -17,10 +17,13 @@ module.exports = {
               [body.username, body.password],
               function (e, entity) {
                   if (e) {
-                      res.status(500).send(e);
+                      res.status(500).send({ errMsg: '服务器异常，请稍后再试' });
                       return;
                   }
 
+                  if (!entity[0]) {
+                      return res.status(403).send({ errMsg: '用户名或密码错误' });
+                  }
                   if (entity[0]) {
                       // eslint-disable-next-line
                       var startTime = new Date().getTime();
@@ -28,7 +31,7 @@ module.exports = {
                       var endDateTime = parseFloat(entity[0].endDateTime);
 
                       if (startTime > endDateTime) {
-                          return res.status(403).send('用户名或密码错误');
+                          return res.status(403).send({ errMsg: '当前状态为：已过期。' + '接入有效期截止' + new Date(endDateTime) });
                       }
 
                       // eslint-disable-next-line
@@ -41,36 +44,57 @@ module.exports = {
 
                       return res.json(token);
                   }
-                  return res.status(403).send('用户名或密码错误');
-
               });
 
         });
 
         app.post('/logs', function (req, res) {
             if (!req.headers || !req.headers.token) {
-                res.status(403).send('Bad Request! Required Parameters: token');
+                res.status(400).send('Bad Request! Required Parameters: token');
                 return;
             }
 
             jwt.verify(req.headers.token, privateKey, function (err, decoded) {
                 if (err) {
-                    return res.status(403).send('Bad Request! Required Parameters: Invalid token', err);
+                    return res.status(403).send({ errMsg: 'Invalid token' });
                 }
-                Logs.add({
-                    domainId: decoded.domainId,
-                    envName: decoded.envName,
-                    level: req.body.level,
-                    timeStamp: req.body.timeStamp,
-                    msg: req.body.msg
-                }, function (e) {
+                global.redisClient.get(decoded.domainId, function (e, status) {
+                    // Domain status is Active
                     if (e) {
-                        return res.status(403).send('Save message error: ', e);
+                        return res.status(500).send('Redis Server Error：', e);
                     }
-                    return res.json({
-                        level: req.body.level
+                    if (status !== '1') {
+                        return res.status(403).send({ errMsg: '对接系统接入状态为：已禁用。' });
+                    }
+                    global.redisClient.get(decoded.domainId + '.endDateTime', function (e2, endDateTime) {
+                        if (e2) {
+                            return res.status(500).send('Redis Server Error：', e);
+                        }
+                        // Domain status is Active
+                        if (endDateTime < new Date().getTime()) {
+                            return res.status(403).send({ errMsg: '系统接入有效期已过期。' + '接入有效期截止' + new Date(endDateTime) });
+                        }
+
+                        Logs.add({
+                            domainId: decoded.domainId,
+                            envName: decoded.envName,
+                            level: req.body.level,
+                            timeStamp: req.body.timeStamp,
+                            msg: req.body.msg
+                        }, function (e3) {
+                            if (e3) {
+                                return res.status(500).send('Save message error: ', e3);
+                            }
+                            global.redisClient.get(decoded.domainId + '.' + decoded.envName + '.logLevel', function (e4, level) {
+                                return res.json({
+                                    level: e4 ? req.body.level : level
+                                });
+                            });
+                        });
+
                     });
                 });
+
             });
 
         });
